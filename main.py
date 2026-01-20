80816abf3825bd833b2a7d913deb7e8352cdda22
HypeAT <empty.page2022@gmail.com>
2026-01-20 06:30:53 +0300

    Main 3: Triangle online

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import pygame
 import constants
+import player
 from logger import log_state
+
+screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
 def main():
     pygame.init()
     pygame.time.Clock()
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    x = constants.SCREEN_WIDTH / 2
+    y = constants.SCREEN_HEIGHT / 2
+    main_screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    triangle = player.Player(x, y)
 
     while True:
         dt = 0
@@ -13,7 +20,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
             log_state()
-            screen.fill("black")
+            main_screen.fill("black")
+            triangle.draw(main_screen)
             pygame.display.flip()
             clock.tick(60)
             dt = clock.tick(60) / 1000

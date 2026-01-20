7322cc21b29e9bd16af9ea82465198afa4e3b3c1
HypeAT <empty.page2022@gmail.com>
2026-01-20 18:45:48 +0300

    Main 7: Meteor shower!!!

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 import player
 from logger import log_state
+from asteroids import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     pygame.init()
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     main_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player.Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+
+    AsteroidField()
     player.Player(x, y)
     clock = pygame.time.Clock()
 

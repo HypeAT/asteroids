6ab4b24a6f31b8628edc27ef4ea68e0f673ec918
HypeAT <empty.page2022@gmail.com>
2026-01-20 17:29:58 +0300

    Main 6: A tiny cleanup

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import pygame
-import constants
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 import player
 from logger import log_state
 
 
 def main():
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
     pygame.init()
-    x = constants.SCREEN_WIDTH / 2
-    y = constants.SCREEN_HEIGHT / 2
-    main_screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-    triangle = player.Player(x, y)
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    main_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player.Player.containers = (updatable, drawable)
+    player.Player(x, y)
     clock = pygame.time.Clock()
 
     while True:
         dt = clock.tick(60) / 1000
         main_screen.fill("black")
-        triangle.update(dt)
-        triangle.draw(main_screen)
+        updatable.update(dt)
+        for drawing in drawable:
+            drawing.draw(main_screen)
         pygame.display.flip()
         log_state()
         for event in pygame.event.get():

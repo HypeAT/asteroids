ad5513bcbd0d99e7441e23c267029b630ebd3407
HypeAT <empty.page2022@gmail.com>
2026-01-20 05:25:50 +0300

    Main 1: Initialize asteroids!!!

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+import constants
+from logger import log_state
+def main():
+    pygame.init()
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            log_state()
+            screen.fill("black")
+            pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
